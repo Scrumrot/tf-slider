2ffc94d2d25641281d6d10e17a869469a7058c9c
Timothy Frank <tim@brainbytescreative.com>
2015-10-13 23:51:52 -0400

    fixes equalHeights
    equalHeights

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 //main.js
 'use strict';
 
-window.onload = function() {
+// window.onload = function() {
 
-    var setItemsWidth = function(itemsInView, itemsMargin, wrapperClass, itemsClass) {
-
-        // arr.map((item, index) => item)
+    var setItemsWidth = function(itemsInView, itemsMargin, wrapperClass) {
         let w = document.getElementsByClassName(wrapperClass);
         let sliders = Array.from(w, el =>
 		    ({
@@ -15,89 +13,47 @@ window.onload = function() {
 		    })
 		);
 		sliders.forEach((wrapper, index, array) => {
+            
+            let tallest = new equalHeights(wrapper.items);
+            tallest.setTallest();
 		    wrapper.items.forEach((item, itemIndex, itemArray) => {
 		        let ci = (itemIndex + 1) % wrapper.itemsInView;
 		        item.style.marginTop = wrapper.itemsInView < (itemIndex + 1) ? wrapper.itemsMargin + '%' : 0;;
 		        item.style.marginLeft = ci === 0 ? wrapper.itemsMargin + '%' : ci === 1 ? 0 : wrapper.itemsMargin + '%';
 		        item.style.width = getItemWidth(wrapper.itemsMargin, wrapper.itemsInView) + '%';
 		        item.style.float = 'left';
+                item.style.minHeight = tallest.getTallest()+'px';
+                item.getElementsByClassName('js-footer')[0].style.marginTop = getFooterMarginTop(item)+'px';
+                
 		    })
 		});
         
-        // var itmz = getElementsArray(wrapper).map((item, index, array, thisArg) => this);
-        
-        // for (var i = 0; i < wrapper.length; i++) {
-          
-          
-        //     var items = wrapper[i].getElementsByClassName(itemsClass);
-        //     var wrapperArea = 100 - (itemsMargin * (itemsInView - 1));
-        //     var itemWidth = wrapperArea / itemsInView;
-        //     for (var z = 0; z < items.length; z++) {
-        //         let ci = (z + 1) % itemsInView;
-        //         let mL = ci === 0 ? itemsMargin + '%' : ci === 1 ? 0 : itemsMargin + '%';
-        //         let mT = itemsInView < (z + 1) ? itemsMargin + '%' : 0;
-        //         items[z].style.marginTop = mT;
-        //         items[z].style.marginLeft = mL;
-        //         items[z].style.width = itemWidth + '%';
-        //         items[z].style.float = 'left';
-
-
-        //     }
-        // }
     };
-    var getElementsArray = function(warpperObj, itemClass) {
-        return Array.prototype.slice.call(warpperClass).map((item, index) => [].slice.call(item.getElementsByClassName(itemClass)));
-        // Array.prototype.filter.call(element, (element)=> element);
+    var getInnerHeight = function(item){
+        let innerTotal = 0; 
+        Array.from(item.children).forEach((innerEl, innerInd, innerArr) =>{innerTotal += innerEl.offsetHeight;}  ); 
+        return innerTotal;
     }
-
-    var setRowMarginTop = function(itemTotal, itemsPerRow, cuerrentItem, marginTop) {
-        if (itemTotal > itemsPerRow && cuerrentItem > itemsPerRow) {
-
-        };
-
+    var getFooterMarginTop = function(item){
+        return item.offsetHeight-(getInnerHeight(item));
     }
+    //gets its info from an itemsArray with somthing equivical to this.style.offsetHeight 
+    // var equalHeights = function(itemsArray){
+    //     let tallest=0;
+    //     itemsArray.forEach((el, ind, arr) => {
+    //         if (el.offsetHeight > tallest){tallest = el.offsetHeight; } 
+    //     });
+    //     return tallest;
+    // }
+    var equalHeights = function(itemsArray){
+        let tallest=0;
+        this.setTallest = ()=> itemsArray.find( (el)=> el.offsetHeight > tallest ? tallest = el.offsetHeight : '');
+        this.getTallest = ()=>  tallest;
+    }
+    //gets its info from settings
     var getItemWidth = function(itemsMargin, itemsInView) {
     return (100 - (itemsMargin * (itemsInView - 1))) / itemsInView;
 	}
     setItemsWidth(4, 2, 'js-wrapper', 'js-item');
     //	end of onload
-};
-
-
-// var getSliders = function(wrapperClass) {
-//     let wrapper = document.getElementsByClassName(wrapperClass);
-//     Array.prototype.slice.call(warpperClass).map((item, index) => {
-//             width:
-//         }
-//         [].slice.call(item.getElementsByClassName(itemClass))); {
-//         width: array[{
-//             style: this.style,
-//             childeren: getElementsArray(this, itemClass)
-//         }]
-//     }
-
 // };
-// var w = document.getElementsByClassName('js-wrapper');
-// var sliders = Array.from(w, el =>
-//     ({
-//         itemsMargin: 2,
-//         itemsInView: 4,
-//         items: (Array.from(el.children))
-//     })
-// );
-
-// sliders.forEach((wrapper, index, array) => {
-//     wrapper.items.forEach((item, itemIndex, itemArray) => {
-//         let ci = (itemIndex + 1) % wrapper.itemsInView;
-//         item.style.marginTop = wrapper.itemsInView < (itemIndex + 1) ? wrapper.itemsMargin + '%' : 0;;
-//         item.style.marginLeft = ci === 0 ? wrapper.itemsMargin + '%' : ci === 1 ? 0 : wrapper.itemsMargin + '%';
-//         item.style.width = getItemWidth(wrapper.itemsMargin, wrapper.itemsInView) + '%';
-//         item.style.float = 'left';
-//     })
-// });
-
-//[].slice.call(wrapper).map(thisArg)=>thisArg.getElementsByClassName('js-item');
-
-
-// Array.prototype.slice.call(tiles).map(function(item, index, array,thisArg ){return [].slice.call(item.getElementsByClassName('js-item'))});
-// Array.prototype.slice.call(tiles).map((item, index) => [].slice.call(item.getElementsByClassName('js-item')));
